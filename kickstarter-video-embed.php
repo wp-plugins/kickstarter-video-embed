f6c7e448e210ff607c95518a2c4a62ebddc81a7c
DrewAPicture <DrewAPicture@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-10 06:38:04 +0000

    Tagging v2.0 of Kickstarter Video embed plugin.
    git-svn-id: https://plugins.svn.wordpress.org/kickstarter-video-embed/tags/2.0@1157152 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kickstarter-video-embed.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Plugin Name: Kickstarter Video Embed
+ * Description: Adds Kickstarter to the list of supported oEmbed providers.
+ * Plugin URI: https://wordpress.org/plugins/kickstarter-video-embed/
+ * Author: Drew Jaynes
+ * Author URI: http://werdswords.com
+ * License: GPLv2 or later
+ * Version: 2.0
+ */
+
+/**
+ * (Maybe) deactivate the plugin for WordPress 4.2+.
+ *
+ * @since 2.0
+ */
+function kve_maybe_deactivate_plugin() {
+	if ( version_compare( get_bloginfo( 'version' ), '4.2', '>=' ) ) {
+
+		if ( current_user_can( 'activate_plugins' ) ) {
+			add_action( 'admin_init', 'kve_deactivate_plugin' );
+			add_action( 'admin_notices', 'kve_deactivate_plugin_notice' );
+		}
+	}
+}
+add_action( 'plugins_loaded', 'kve_maybe_deactivate_plugin' );
+
+/**
+ * Register the Kickstarter oEmbed endpoint.
+ *
+ * @since 1.0
+ */
+function kve_register_oembed_provider() {
+	wp_oembed_add_provider( '#https://(www.)?kickstarter.com/projects/.*#i', 'http://www.kickstarter.com/services/oembed', true );
+}
+add_action( 'plugins_loaded', 'kve_register_oembed_provider' );
+
+/**
+ * Utility method to self-deactivate for WordPress 4.2+.
+ *
+ * @since 2.0
+ */
+function kve_deactivate_plugin() {
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+}
+
+/**
+ * Utility method to display a self-deactivation notice for WordPress 4.2+.
+ *
+ * @since 2.0
+ */
+function kve_deactivate_plugin_notice() {
+	?>
+	<div class="updated notice is_dismissable">
+		<p>WordPress now <a href="https://codex.wordpress.org/Embeds#Okay.2C_So_What_Sites_Can_I_Embed_From.3F">supports</a> Kickstarter oEmbeds as of 4.2. The <strong>Kickstarter Video Embed</strong> has been automatically <strong>deactivated</strong>.</p>
+	</div>
+	<?php
+	if ( isset( $_GET['activate'] ) ) {
+		unset( $_GET['activate'] );
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Kickstarter Video Embed ===
+Contributors: DrewAPicture, 10up
+Donate link: http://wordpressfoundation.org/donate/
+Tags: kickstarter, oembed, embed
+Requires at least: 2.9.0
+Tested up to: 4.2.2
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds Kickstarter to the list of supported oEmbed providers. Kickstarter oEmbeds were added to 4.2, so this becomes moot.
+
+== Description ==
+
+<strong>This plugin is unnecessary beyond WordPress 4.2 as Kickstarter was added to the core [oEmbeds list](https://codex.wordpress.org/Embeds#Okay.2C_So_What_Sites_Can_I_Embed_From.3F) providers list.</strong>
+This plugin allows you to more easily embed [Kickstarter](https://www.kickstarter.com/) project videos on your WordPress site.
+
+To embed a project video, simply paste the URL to your Kickstarter project on its own line in the editor. WordPress will do the rest.
+
+If the project has no video, Kickstarter will instead send a widget with the project image, status, description, and some stats.
+
+== Installation ==
+
+1. Search for 'Kickstarter Video Embed' from the Plugins > Add New screen.
+2. Install and activate the plugin.
+
+== Screenshots ==
+
+1. Kickstarter embed preview in the editor (4.0+).
+2. Kickstarter embed link in the editor (3.9-).
+3. Kickstarter embed displayed on the front of the site.
+
+== Changelog ==
+
+= 2.0 =
+
+* Added self-deactivation for WordPress 4.2+
+
+= 1.0 =
+
+* First version, registers the oEmbed endpoint for project videos.
